789c6f5fe9314b7a7b9104d1ce4e9ff151767b28
Adilsonss <pradilsonsantana@outlook.com>
2026-01-30 18:55:54 -0300

    Melhorar validacao de URL e template do formulario de produtos

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,7 +1,14 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, FloatField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Optional, URL
+from wtforms.validators import DataRequired, Optional, Regexp
+
+
+def url_or_empty(form, field):
+    """Validate that field is either empty or a valid URL"""
+    if field.data:
+        if not (field.data.startswith("http://") or field.data.startswith("https://")):
+            raise ValueError("URL deve começar com http:// ou https://")
 
 
 class ItemForm(FlaskForm):
@@ -14,7 +21,7 @@ class ItemForm(FlaskForm):
     )
     image_url = StringField(
         "Or Image URL",
-        validators=[Optional(), URL()],
+        validators=[Optional(), url_or_empty],
     )
     details = TextAreaField("Description", validators=[DataRequired()])
     price_id = StringField("Stripe Price ID", validators=[Optional()])
